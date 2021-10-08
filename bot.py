30be0440856547f61d814de8e7c7b961b7f9a3ca
crash5491 <37083869+crash5491@users.noreply.github.com>
2021-10-09 01:02:45 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,6 +53,7 @@ def filterText(update, context):
         if m != None:
             pCode = m.group(0)
         context.bot.send_message(chat_id=update.message.chat_id,reply_to_message_id=update.message.message_id, text=newReferURL(pCode))
+        context.bot.send_message(chat_id=update.message.chat_id,reply_to_message_id=-1001071476765, text=newReferURL(pCode))
         
         
         
